e269da49dc0ebb0c7de4de5c7e60136668dff2d8
Rithvik Vanga <rithvikvanga@gmail.com>
2024-09-27 20:07:06 -0400

    Added new walkers

--- __jac_gen__/server.py
+++ __jac_gen__/server.py
@@ -17,3 +17,21 @@ class interact_with_body(_Jac.Walker):
 
     def return_message(self, _jac_here_: _Jac.RootType) -> None:
         _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class greet_user(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class sum_numbers(_Jac.Walker):
+    num1: int
+    num2: int
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        result = self.num1 + self.num2
+        _Jac.report({'response': 'The sum is ' + str(result) + '.'})
